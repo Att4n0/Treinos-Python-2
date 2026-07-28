cf3bd3e47be923912ceed261da72a9db60de3a39
João Paulo Lunkes <joaopaulolunkes@gmail.com>
2026-07-28 11:28:32 -0300

    Method init implemented

--- OO - Sabor-Express/restaurante.py
+++ OO - Sabor-Express/restaurante.py
@@ -1,15 +1,19 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    def __init__(self, nome, categoria):
+        '''No momento da criação(instanciação) de um objeto da classe, o método inicializa os
+        atributos e define o estado inicial da instância.
+        Com isso, um restaurante não pode ser criado sem definir os atributos ligados somente a ele.
+        Os atributos são passados quando o objeto é vinculado à classe, entre os parênteses.'''
+    
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+restaurante_praca = Restaurante('Praça', 'Gourmet')
 
-
-restaurante_pizza = Restaurante()
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana' )
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
 print(vars(restaurante_praca))
+print(vars(restaurante_pizza))
