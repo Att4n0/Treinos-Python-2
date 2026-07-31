08f578a23bebfcae33eb5a0e47c5bdebc8e16b0e
João Paulo Lunkes <joaopaulolunkes@gmail.com>
2026-07-30 22:00:59 -0300

    Created class methods, properties and lost my mind several times in the process...

--- OO - Sabor-Express/restaurante.py
+++ OO - Sabor-Express/restaurante.py
@@ -33,22 +33,27 @@ class Restaurante:
         - Uma string
 
         '''
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
 
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         #Método criado
         '''Quando chamado, o método vai listar automaticamente todos os objetos na lista restaurantes'''
         print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | Status')
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante._ativo}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+            #Preste atenção. ali no restaurante. ativo, não queremos o true or false. Se colocar o _ativo, ele vai no atributo de instância lá na fonte,
+            #e não vai mostrar os emojis. Se colocar ativo, esse é o nome 
 
     @property
     def ativo(self):
         return '✅' if self._ativo else '❎'
 
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
-
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana' )
 
 Restaurante.listar_restaurantes()
