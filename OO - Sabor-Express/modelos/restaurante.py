6e8f1b020ae8df1588d02ba0e6dea687f8e8a286
João Paulo Lunkes <joaopaulolunkes@gmail.com>
2026-08-01 21:41:48 -0300

    Improved listar_restaurantes, created new property for restaurant ratings

--- OO - Sabor-Express/modelos/restaurante.py
+++ OO - Sabor-Express/modelos/restaurante.py
@@ -1,3 +1,6 @@
+from modelos.avaliacao import Avaliacao
+'''Importa a classe Avaliacao do arquivo avaliacao. Essa classe vai interagir com a classe Restaurante'''
+
 class Restaurante:
 
     restaurantes = []
@@ -18,6 +21,7 @@ class Restaurante:
         self._nome = nome.title()
         self._categoria = categoria.upper()
         self._ativo = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -38,17 +42,69 @@ class Restaurante:
     @classmethod
     def listar_restaurantes(cls):
         #Método criado
-        '''Quando chamado, o método vai listar automaticamente todos os objetos na lista restaurantes'''
-        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | Status')
+        '''Quando chamado, o método vai listar automaticamente todos os objetos na lista restaurantes
+        
+            Repare que na parte de restaurante ativo, chama-se .ativo, e não ._ativo.
+            Assim, chama-se a propriedade ativo (abaixo), que retorna os emojis ao invés de False e True.
+            O ^25 faz com que tenha um espaço de 25 caracteres para a string, e ela fique centralizada nesse espaço.
+
+        Inputs
+        -nome dos restaurantes (str)
+        -categorias dos restaurantes (str)
+        -média das avaliações (float)
+        -emojis de ativo (str)
+
+        Output:
+        -print com as informações dos restaurantes
+        
+        '''
+        print(f'{'Nome do restaurante':^25} | {'Categoria':^25} | {'Nota':^25} | {'Status':^25}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
-            #Preste atenção. ali no restaurante. ativo, não queremos o true or false. Se colocar o _ativo, ele vai no atributo de instância lá na fonte,
-            #e não vai mostrar os emojis. Se colocar ativo, esse é o nome 
+            print(f'{restaurante._nome:^25} | {restaurante._categoria:^25} | {restaurante.media_avaliacoes:^25} | {restaurante.ativo:^25}')
 
     @property
     def ativo(self):
+        '''Propriedade "getter":
+            se o restaurante estiver ativo, retorna como string o emoji ✅
+            se or restaurante não estiver ativo, retorna o emoji ❌
+        
+            Útil para mostrar em listas. Ver função listar restaurantes
+        '''
         return '✅' if self._ativo else '❌'
 
     def alternar_estado(self):
+        '''Alterna o estado do restaurante de ativo para não ativo, ou vice-versa'''
         self._ativo = not self._ativo
 
+    def receber_avaliacao(self, cliente, nota):
+        '''Adiciona as avaliações de clientes, incluindo o nome do cliente e nota, a uma lista ligada ao objeto.
+        
+        Input:
+        -cliente (str) (Importado)
+        -nota (int) (Importado)
+
+        Output:
+        -Adiciona à lista individual do restaurante a avaliação.
+        '''
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
+
+    @property
+    def media_avaliacoes(self):
+        '''Propriedade que calcula a média das notas para um restaurante.
+           Se não há avaliações, a média é 0.
+        
+        Inputs:
+        - nota (int) (Importado)
+        - avaliação (list)
+
+        Output:
+        - média (float)
+
+        '''
+        if not self._avaliacao:
+            return 0
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas, 1)
+        return media
